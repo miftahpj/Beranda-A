99256914761eff62bb2af554f2f3b13f7a0e8118
miftahpj <142276212+miftahpj@users.noreply.github.com>
2024-12-16 13:15:50 +0700

    Add files via upload

---
+++ pages/profil/ds.vue
@@ -0,0 +1,21 @@
+<template>
+    <title>Data Statistic</title>
+
+<div class="container-fluid mt-5">
+
+    <div class="card text-center">
+  <div class="card-header">
+   <h3>Data Statistic SMKN 4 Tasikmalaya</h3>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-body-secondary">
+    2 days ago
+  </div>
+    </div>
+</div>
+
+</template>
